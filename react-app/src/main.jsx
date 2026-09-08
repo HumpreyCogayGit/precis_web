6bcb2871022112081dd92bd68a9b4275494b2694
Humprey Cogay <humprey.cogay@gmail.com>
2026-09-09 00:20:57 +0800

    add vercel analytics

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App.jsx';
@@ -27,6 +28,7 @@ root.render(
       </Routes>
       <CookieConsent />
     </BrowserRouter>
+    <Analytics />
   </React.StrictMode>
 );
 
