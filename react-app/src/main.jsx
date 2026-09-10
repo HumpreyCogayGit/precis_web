e7d177b0f8ac85550fcf7ca3c98a9adec929971c
Humprey Cogay <humprey.cogay@gmail.com>
2026-09-11 00:02:09 +0800

    add vercel speed plugin

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App.jsx';
@@ -29,6 +30,7 @@ root.render(
       <CookieConsent />
     </BrowserRouter>
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
 
